fddcc2d1da09c219f865c7c5ea5e01f4573ab2bf
ZooMG1N <zoomg1next@gmail.com>
2018-01-13 01:49:11 +0200

    add interactivity

--- js/toTop.js
+++ js/toTop.js
@@ -1,24 +1,15 @@
-$(document).ready(function () {
-    $('.zzz').click(function () {
-        var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top;
-            $('html').animate({ scrollTop: destination }, 1100);
+$(document).ready(function(){   
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.to-Top').fadeIn();
+        } else {
+            $('.to-Top').fadeOut();
+        }
+    });
+    $('.to-Top').click(function () {
+        $('html').animate({
+            scrollTop: 0
+        }, 1000);
+       
     });
 });
-
-
-//$(document).ready(function(){   
-//    $(window).scroll(function () {
-//        if ($(this).scrollTop() > 0) {
-//            $('#scroller').fadeIn();
-//        } else {
-//            $('#scroller').fadeOut();
-//        }
-//    });
-//    $('#scroller').click(function () {
-//        $('body,html').animate({
-//            scrollTop: 0
-//        }, 1000);
-//        return false;
-//    });
-//});
