dfd956947906581653962823f21a42e67710638d
Sedrik666 <fedvetal@mail.ru>
2018-01-30 03:24:35 +0200

    remove jquery for gallery and class detailed-gallery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-
-    var gallery = $(".portfolio-gallery");
-    var galleryElem = $(".examples");
-    var iconSites = $(".icon-site");
-    var timeoutId;
-
     $(".logo-menu").click(function () {
         $(".hatchet").toggleClass("hatchetRotate");
         $(".hammer").toggleClass("hammerRotate");
@@ -12,52 +6,7 @@ $(document).ready(function () {
         $(".header").toggleClass("header-small");
     })
 
-    galleryElem.click(function () {
-        if ($(this).hasClass("activeElem")) {
-            gallery.removeClass("gallery-detailed") && gallery.addClass("gallery-normal");
-            $(this).removeClass("activeElem");
-        }
-
-        else {
-            gallery.addClass("gallery-detailed") && gallery.removeClass("gallery-normal");
-            galleryElem.each(function () {
-                $(this).removeClass("activeElem");
-            });
-            $(this).insertBefore($(".portfolio-gallery>div:first-child"));
-            $(this).toggleClass("activeElem");
-        }
-    });
-
-    iconSites.hover(
-        function () {
-            var current = $(this);
-            if (!timeoutId) {
-                timeoutId = window.setTimeout(function () {
-                    timeoutId = null;
-                    current.addClass("showScroll");
-                }, 1000);
-            }
-        },
-        function () {
-            var current = $(this);
-            if (timeoutId) {
-                window.clearTimeout(timeoutId);
-                timeoutId = null;
-            }
-            else {
-                current.removeClass("showScroll");
-            }
-        }
-    )
-
-    $("#contact .user-message").on('input', function(){
-        $(".charLeft").text(600-$(this).val().length);
-       ($(".charLeft").text())<180?
-            $(".counter").addClass("counterVisible"):
-            $(".counter").removeClass("counterVisible");
-    });
-
-//feedback
+    //feedback
     var reviewWidth = $(".review").outerWidth();
     var reviewHeight = $(".review").outerHeight();
     var iteration=0;
