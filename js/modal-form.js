28de218c1d099446dc472edf8403fb8f942e1cda
Sedrik666 <fedvetal@mail.ru>
2018-02-01 14:36:39 +0200

    modal callback edit

--- js/modal-form.js
+++ js/modal-form.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 //write us modal menu start
 	$('#write-us, .call-back-link').click( function(){  
 		$('#overlay').fadeIn(400, function(){
-			$('.call-back-form-header').css('display', 'grid').animate({opacity: 1, top: '10%',left: '35%'}, 350);
+			$('.call-back-form-modal').css('display', 'grid').animate({opacity: 1, top: '10vh'}, 350);
 		});
 	});
 
     $('#overlay, .close-form, .examples-detailed:after').click( function(){ // лoвим клик пo крестику или пoдлoжке
-        $('.call-back-form-header').animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+        $('.call-back-form-modal').animate({opacity: 0, top: '100%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
             function(){ // пoсле aнимaции
                 $(this).css('display', 'none'); // делaем ему display: none;
                 $('#overlay').fadeOut(400); // скрывaем пoдлoжку
