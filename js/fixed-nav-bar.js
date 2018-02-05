b8eb492f5f56965f99e618d2b0ee757deb3caa43
Sedrik666 <fedvetal@mail.ru>
2018-02-05 22:06:31 +0200

    new button for fixed heading

--- js/fixed-nav-bar.js
+++ js/fixed-nav-bar.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
     $(window).scroll(function () {
-        if($(document).scrollTop()<$("#header").outerHeight()){
-            $("#header-show").fadeOut("fast");
+        if($(window).outerWidth()>810){
+            if($(document).scrollTop()>$("#header").outerHeight()){
+                $(".logo-menu").addClass("fixedLogo");
+            }
+            else{
+                $(".logo-menu").removeClass("fixedLogo");
+            }
         }
         else{
-            $("#header-show").fadeIn("slow");
+
+            if($(document).scrollTop()<$("#header").outerHeight()){
+                $("#header-show").fadeOut("fast");
+            }
+            else{
+                $("#header-show").fadeIn("slow");
+            }
         }
+
     });
-    $("#header-show").click(function () {
-        if ($("#header-show-check").is(':checked')){
-            $('.nav-bar')
-                .css('position', 'fixed')
-                .css('width', '100%')
-                .css('z-index', '10')
-                .css('background-color', 'black')
-                .css('opacity', '1');
-        } else {
-            $('.nav-bar')
-                .css('position', 'relative')
-                .css('background-color', 'black')
-                .css('opacity', '0.8');
-        }
+    $("#header-show, .logo-menu").click(function () {
+            $('.nav-bar').toggleClass("fixedNav")
+            $(".logo-menu").toggleClass("fixedLogo");
     });
 });
 
