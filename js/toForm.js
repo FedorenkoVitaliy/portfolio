90ed6338ce44662b94dbbd0e917555bb41d18f4d
Sedrik666 <fedvetal@mail.ru>
2018-02-17 21:00:04 +0200

    refactoring, replacing html and fix small bags

--- js/toForm.js
+++ js/toForm.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.to-about-us, .to-advantages, .to-portfolio, .to-team, .to-form, .to-footer').click(function () {
+    $('.to-about-us, .to-advantages, .to-portfolio, .to-team, .to-contact, .to-reviews').click(function () {
         var elementClick = $(this).attr("href");
         var destination = $(elementClick).offset().top;
             $('html').animate({ scrollTop: destination }, 2000);
