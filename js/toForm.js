3dee5f694916c6ecdcb82e30b85615437d1d0047
Sedrik666 <fedvetal@mail.ru>
2018-02-18 16:25:55 +0200

    easy refactoring

--- js/toForm.js
+++ js/toForm.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.to-about-us, .to-advantages, .to-portfolio, .to-team, .to-contact, .to-reviews').click(function () {
         var elementClick = $(this).attr("href");
         var destination = $(elementClick).offset().top;
-            $('html').animate({ scrollTop: destination }, 2500);
+        var navbarHeight = $(".nav-bar").outerHeight();
+            $('html').animate({ scrollTop: destination-navbarHeight}, 2500);
     });
 });
